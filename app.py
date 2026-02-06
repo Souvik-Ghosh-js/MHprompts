6c3f2602652b8f0f4bb70ee661fadfa5da192196
Souvik Ghosh <souvik_ghosh@pursuitsoftware.com>
2026-02-06 22:53:53 +0530

    gg

--- app.py
+++ app.py
@@ -355,4 +355,5 @@ def count_l2_by_l1():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
